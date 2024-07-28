5f4329ee465e3fddfdb07bdc8e7435f2b842cb8c
Dima Ryazanov <dima@gmail.com>
2024-07-28 16:24:26 -0700

    Switch to a custom crc32q implementation

--- src/jdmtool/crc32q.py
+++ src/jdmtool/crc32q.py
@@ -1,4 +1,38 @@
-import libscrc
+from typing import List
 
-def calculate_crc32_q(data: bytes, initial_value: int = 0) -> int:
-    return libscrc.crc32_q(data, initial_value)  # pylint: disable=no-member
+CRC32Q_POLYNOMIAL = 0x814141AB
+
+
+def _create_crc32_q_lookup_table() -> List[int]:
+    lookup_table: List[int] = []
+    for index in range(256):
+        value = index << 24
+        for _ in range(8):
+            if value & (1 << 31):
+                value = ((value << 1) & 0xFFFFFFFF) ^ CRC32Q_POLYNOMIAL
+            else:
+                value <<= 1
+
+        lookup_table.append(value)
+
+    return lookup_table
+
+
+_crc32_q_lookup_table = _create_crc32_q_lookup_table()
+
+
+def calculate_crc32_q(data: bytes, value: int = 0) -> int:
+    for b in data:
+        index = b ^ (value >> 24)
+        value = _crc32_q_lookup_table[index] ^ ((value & 0x00FFFFFF) << 8)
+    return int(value)  # Handle the numpy case
+
+
+try:
+    import numpy as np  # type: ignore
+    from numba import jit  # type: ignore
+
+    _crc32_q_lookup_table = np.array(_crc32_q_lookup_table)
+    calculate_crc32_q = jit(nopython=True, nogil=True)(calculate_crc32_q)
+except ImportError as ex:
+    print("Using a slow crc32q implementation; consider installing jdmtool[jit]")
