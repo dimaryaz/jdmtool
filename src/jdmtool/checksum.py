f9b53f03bb9f1e50b2e65f6a1ab389b67d4b6be0
Dima Ryazanov <dima@gmail.com>
2024-08-13 23:00:28 -0700

    Switch from libscrc to a custom crc32q implementation

--- src/jdmtool/checksum.py
+++ src/jdmtool/checksum.py
@@ -1,4 +1,39 @@
-import libscrc
+from typing import List
 
-def crc32q_checksum(data: bytes, initial_value: int = 0) -> int:
-    return libscrc.crc32_q(data, initial_value)  # pylint: disable=no-member
+
+CRC32Q_POLYNOMIAL = 0x814141AB
+
+
+def _create_lookup_table(polynomial: int) -> List[int]:
+    lookup_table: List[int] = []
+    for index in range(256):
+        value = index << 24
+        for _ in range(8):
+            if value & (1 << 31):
+                value = ((value << 1) & 0xFFFFFFFF) ^ polynomial
+            else:
+                value <<= 1
+
+        lookup_table.append(value)
+
+    return lookup_table
+
+
+_crc32q_lookup_table = _create_lookup_table(CRC32Q_POLYNOMIAL)
+
+
+def crc32q_checksum(data: bytes, value: int = 0) -> int:
+    for b in data:
+        index = b ^ (value >> 24)
+        value = _crc32q_lookup_table[index] ^ ((value & 0x00FFFFFF) << 8)
+    return value
+
+
+try:
+    import numpy as np  # type: ignore
+    from numba import jit  # type: ignore
+
+    _crc32q_lookup_table = np.array(_crc32q_lookup_table)
+    crc32q_checksum = jit(nopython=True, nogil=True)(crc32q_checksum)
+except ImportError as ex:
+    print("Using a slow crc32q implementation; consider installing jdmtool[jit]")
