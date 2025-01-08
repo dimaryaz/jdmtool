7b5e5341b346c503ee9801e2aed1221aed99980a
Dima Ryazanov <dima@gmail.com>
2025-01-07 23:27:25 -0800

    Clean up config

--- src/jdmtool/config.py
+++ src/jdmtool/config.py
@@ -1,6 +1,6 @@
 from configparser import ConfigParser
+from functools import cache
 import pathlib
-from typing import Optional
 
 from .common import get_config_dir
 
@@ -20,21 +20,15 @@ def get_config_file() -> pathlib.Path:
     return get_config_dir() / CONFIG_FILE
 
 
-_CONFIG: Optional[ConfigParser] = None
-
-
+@cache
 def get_config():
-    global _CONFIG
-
-    if _CONFIG is None:
-        config = ConfigParser()
-        try:
-            with open(get_config_file(), encoding="utf-8") as fd:
-                config.read_file(fd)
-        except FileNotFoundError:
-            config.read_dict(DEFAULT_CONFIG)
-            with open(get_config_file(), 'w', encoding="utf-8") as fd:
-                config.write(fd)
-        _CONFIG = config
+    config = ConfigParser()
+    try:
+        with open(get_config_file(), encoding="utf-8") as fd:
+            config.read_file(fd)
+    except FileNotFoundError:
+        config.read_dict(DEFAULT_CONFIG)
+        with open(get_config_file(), 'w', encoding="utf-8") as fd:
+            config.write(fd)
 
     return config
