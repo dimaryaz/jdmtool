dimaryaz/jdmtool

commit 525dcd995f3ea69058c02f03cbe4978423edce1e

Very experimental, unfinished support for TAWS cards

src/jdmtool/data_card/skybound.py
@@ -78,7 +78,7 @@ class SkyboundDevice(ProgrammingDevice):
         if info is None:
             raise ProgrammingException(f"Unknown data card with chip IIDs: {hex_iids}. Please file a bug!")
 
-        (self.sectors_per_chip, self.card_info) = info
+        (self.card_type, self.sectors_per_chip, self.card_info) = info
 
     def get_firmware_version(self) -> str:
         self.bulk_write(b"\x60")
@@ -161,7 +161,7 @@ class SkyboundDevice(ProgrammingDevice):
         self.set_led(i % 2 == 0)
         self.check_card()
 
-    def read_blocks(self, start_sector: int, num_sectors: int) -> Generator[bytes, bool, None]:
+    def read_blocks(self, start_sector: int, length: int) -> Generator[bytes, None, None]:
         self.before_read()
         for sector in range(start_sector, start_sector + num_sectors):
             self.select_sector(sector)
@@ -178,8 +178,8 @@ class SkyboundDevice(ProgrammingDevice):
             yield
 
     def write_blocks(
-        self, start_sector: int, num_sectors: int,
-        read_func: Callable[[], bytes]
+        self, start_sector: int, length: int,
+        read_func: Callable[[int], bytes]
     ) -> Generator[None, None, None]:
         self.before_write()
         for sector in range(start_sector, start_sector + num_sectors):

src/jdmtool/data_card/common.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from collections.abc import Callable, Generator
+from enum import Enum
 from typing import TYPE_CHECKING
 
 from ..common import JdmToolException
@@ -10,29 +11,47 @@ if TYPE_CHECKING:
     from usb1 import USBDeviceHandle
 
 
-# (manufactorer_id, chip_id) -> (sectors_per_chip, description)
+class DataCardType(Enum):
+    def __init__(self, sector_size: int, read_size, min_write_size: int, max_write_size: int):
+        self.sector_size = sector_size
+        self.read_size = read_size
+        self.min_write_size = min_write_size
+        self.max_write_size = max_write_size
+
+    NONE    = (0x0,     0x0,    0x0,    0x0)
+    NAVDATA = (0x10000, 0x1000, 0x1000, 0x1000)
+    TAWS    = (0x10800, 0xf800, 0x0840, 0xffc0)
+
+
+# (manufactorer_id, chip_id) -> (card_type, sectors_per_chip, description)
 IID_MAP = {
     # 032: 2 MB Intel Series 2 (1 MB x 2)
     # 033: 3 MB Intel Series 2 (1 MB x 3)
     # 034: 4 MB Intel Series 2 (1 MB x 4)
-    (0x89, 0xa2): (0x10, "non-WAAS (white)"),
+    (0x89, 0xa2): (DataCardType.NAVDATA, 0x10, "non-WAAS (white)"),
     #      2 MB Intel          (1 MB x 2)
     #      3 MB Intel          (1 MB x 3)
     #      4 MB Intel          (1 MB x 4)
-    (0x89, 0xa6): (0x10, "non-WAAS (white)"),
+    (0x89, 0xa6): (DataCardType.NAVDATA, 0x10, "non-WAAS (white)"),
 
     # 421: 4 MB AMD Series C/D (2 MB x 2)
     # 431: 6 MB AMD Series C/D (2 MB x 3)
     # 441: 8 MB AMD Series C/D (2 MB x 4)
-    (0x01, 0xad): (0x20, "non-WAAS (green)"),
+    (0x01, 0xad): (DataCardType.NAVDATA, 0x20, "non-WAAS (green)"),
     #      4 MB Intel Series   (2 MB x 2)
     #      6 MB Intel Series   (2 MB x 3)
     #      8 MB Intel Series   (2 MB x 4)
-    (0x89, 0xaa): (0x20, "non-WAAS (green)"),
+    (0x89, 0xaa): (DataCardType.NAVDATA, 0x20, "non-WAAS (green)"),
 
     # 451: 16MB AMD Series C/D (4 MB x 4)
-    (0x01, 0x41): (0x40, "WAAS (silver)"),
-    (0x89, 0x7e): (0x40, "WAAS (orange)"),
+    (0x01, 0x41): (DataCardType.NAVDATA, 0x40, "WAAS (silver)"),
+    (0x89, 0x7e): (DataCardType.NAVDATA, 0x40, "WAAS (orange)"),
+
+    # 128MB
+    (0xec, 0x79): (DataCardType.TAWS, 0x800, "Terrain/Obstacles"),
+
+    # 256MB
+    (0xec, 0xda): (DataCardType.TAWS, 0x1000, "Terrain/Obstacles"),
 }
 
 
@@ -65,10 +84,7 @@ class BasicUsbDevice():
 
 
 class ProgrammingDevice(BasicUsbDevice):
-    BLOCK_SIZE = 0x1000
-    BLOCKS_PER_SECTOR = 0x10
-    SECTOR_SIZE = BLOCK_SIZE * BLOCKS_PER_SECTOR  # 64KB
-
+    card_type = DataCardType.NONE
     chips = 0
     sectors_per_chip = 0x0
     card_info = ""
@@ -86,7 +102,7 @@ class ProgrammingDevice(BasicUsbDevice):
         return f"{self.chips * self.sectors_per_chip // 0x10}MB {self.card_info}"
 
     def get_card_description(self) -> str:
-        return f"{self.chips} chips of {self.sectors_per_chip//0x10}MB"
+        return f"{self.chips} chip(s) of {self.sectors_per_chip//0x10}MB"
 
     def get_firmware_version(self) -> str:
         raise NotImplementedError()
@@ -104,18 +120,24 @@ class ProgrammingDevice(BasicUsbDevice):
         return self.chips * self.sectors_per_chip
 
     def get_total_size(self) -> int:
-        return self.get_total_sectors() * self.SECTOR_SIZE
+        return self.get_total_sectors() * self.card_type.sector_size
 
-    def read_blocks(self, start_sector: int, num_sectors: int) -> Generator[bytes, bool, None]:
+    def pad_for_write(self, block: bytes) -> bytes:
+        rem = len(block) % self.card_type.min_write_size
+        if rem != 0:
+            block += b'\xFF' * (self.card_type.min_write_size - rem)
+        return block
+
+    def read_blocks(self, start_sector: int, length: int) -> Generator[bytes, None, None]:
         raise NotImplementedError()
 
     def erase_sectors(self, start_sector: int, num_sectors: int) -> Generator[None, None, None]:
         raise NotImplementedError()
 
     def write_blocks(
-        self, start_sector: int, num_sectors: int,
-        read_func: Callable[[], bytes]
-    ) -> Generator[None, None, None]:
+        self, start_sector: int, length: int,
+        read_func: Callable[[int], bytes]
+    ) -> Generator[bytes, None, None]:
         raise NotImplementedError()
 
     def check_card(self):

src/jdmtool/data_card/garmin.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from struct import unpack
 from typing import BinaryIO
 
-from .common import IID_MAP, BasicUsbDevice, ProgrammingDevice, ProgrammingException
+from .common import IID_MAP, BasicUsbDevice, DataCardType, ProgrammingDevice, ProgrammingException
 
 
 FIRMWARE_DIR = Path(__file__).parent / 'firmware'
@@ -67,8 +67,6 @@ class GarminProgrammingDevice(ProgrammingDevice):
         card_id = self.get_card_id()
         if card_id == self.NO_CARD:
             raise ProgrammingException("Card is missing!")
-        if card_id == 0x0101daec or card_id == 0x010179ec:
-            raise ProgrammingException("TAWS/Terrain is not (yet) supported")
 
         self.chips = (card_id & 0x00ff0000) >> 16
         manufacturer_id = card_id & 0xff
@@ -78,7 +76,7 @@ class GarminProgrammingDevice(ProgrammingDevice):
         if info is None:
             raise ProgrammingException(f"Unknown data card ID: 0x{card_id:08x}. Please file a bug!")
 
-        (self.sectors_per_chip, self.card_info) = info
+        (self.card_type, self.sectors_per_chip, self.card_info) = info
 
         self.end_read()
         self.end_write()
@@ -95,16 +93,43 @@ class GarminProgrammingDevice(ProgrammingDevice):
     def begin_erase(self, start_sector: int, sector_count: int) -> None:
         self.check_card()
 
+        # Doesn't seem to make a difference, but this is what Garmin's software does.
+        if self.card_type == DataCardType.TAWS:
+            unknown1 = 3
+            unknown2 = 2
+        else:
+            unknown1 = 0
+            unknown2 = 1
+
+        unknown1_bytes = unknown1.to_bytes(2, 'big')
         start_sector_byte = start_sector.to_bytes(2, 'big')
         sector_count_byte = sector_count.to_bytes(2, 'big')
-        buf = b"\x00\x00" + start_sector_byte + b"\x00\x00\x00\x00" + sector_count_byte + b"\x00\x01\x00\x00"
+        unknown2_bytes = unknown2.to_bytes(2, 'big')
+
+        buf = (
+            unknown1_bytes + start_sector_byte + b"\x00\x00\x00\x00" +
+            sector_count_byte + unknown2_bytes + b"\x00\x00"
+        )
         self.control_write(0x40, 0x85, 0x0000, 0x0000, buf)
 
     def begin_write(self, start_sector: int) -> None:
         self.check_card()
 
+        # Doesn't seem to make a difference, but this is what Garmin's software does.
+        if self.card_type == DataCardType.TAWS:
+            unknown1 = 5
+            unknown2 = 8
+        else:
+            unknown1 = 4
+            unknown2 = 0
+
+        unknown1_bytes = unknown1.to_bytes(2, 'big')
         start_sector_byte = start_sector.to_bytes(2, 'big')
-        buf = b"\x00\x04" + start_sector_byte + b"\x00\x00\x00\x00\x00\x00"
+        # Not clear if this is actually an offset, or what is supported.
+        sector_offset_bytes = (0).to_bytes(2, "big")
+        unknown2_bytes = unknown2.to_bytes(2, 'big')
+
+        buf = unknown1_bytes + start_sector_byte + sector_offset_bytes + b"\x00\x00" + unknown2_bytes
         self.control_write(0x40, 0x86, 0x0000, 0x0000, buf)
 
     def end_write(self) -> None:
@@ -113,18 +138,34 @@ class GarminProgrammingDevice(ProgrammingDevice):
     def begin_read(self, start_sector: int) -> None:
         self.check_card()
 
-        start_sector_byte = start_sector.to_bytes(2, 'big')
-        buf = b"\x00\x04" + start_sector_byte + b"\x00\x00\x00\x00\x00\x00"
+        # Doesn't seem to make a difference, but this is what Garmin's software does.
+        unknown = 0 if self.card_type == DataCardType.TAWS else 4
+        unknown_bytes = unknown.to_bytes(2, 'big')
+
+        start_sector_bytes = start_sector.to_bytes(2, 'big')
+
+        # We can technically read from the middle of a sector.
+        # For NavData cards, it's the offset in bytes, 0x0 through 0xffff.
+        # But TAWS cards have a 0x10800 sector size, which doesn't fit in two bytes -
+        # so instead, it's scaled down, i.e., (offset * 0x10000 // 0x10800).
+        # We're not going to bother with this craziness, and will only support 0.
+        sector_offset_bytes = (0).to_bytes(2, "big")
+
+        buf = unknown_bytes + start_sector_bytes + sector_offset_bytes + b"\x00\x00\x00\x00"
         self.control_write(0x40, 0x81, 0x0000, 0x0000, buf)
 
     def end_read(self) -> None:
         self.control_write(0x40, 0x83, 0x0000, 0x0000, b"")
 
-    def read_blocks(self, start_sector: int, num_sectors: int) -> Generator[None, bytes, None]:
+    def read_blocks(self, start_sector: int, length: int) -> Generator[bytes, None, None]:
+        block_size = self.card_type.read_size
+
         self.begin_read(start_sector)
         try:
-            for _ in range(num_sectors * self.BLOCKS_PER_SECTOR):
-                yield self.bulk_read(self.BLOCK_SIZE)
+            while length > 0:
+                block = self.bulk_read(block_size)
+                yield block[:min(block_size, length)]
+                length -= block_size
         finally:
             self.end_read()
 
@@ -142,14 +183,20 @@ class GarminProgrammingDevice(ProgrammingDevice):
             self.end_write()
 
     def write_blocks(
-        self, start_sector: int, num_sectors: int,
-        read_func: Callable[[], bytes]
-    ) -> Generator[None, None, None]:
+        self, start_sector: int, length: int,
+        read_func: Callable[[int], bytes]
+    ) -> Generator[bytes, None, None]:
+        block_size = self.card_type.max_write_size
+
         self.begin_write(start_sector)
         try:
-            for _ in range(num_sectors * self.BLOCKS_PER_SECTOR):
-                block = read_func()
-                self.bulk_write(block)
-                yield
+            while length > 0:
+                read_size = min(block_size, length)
+                block = read_func(read_size)
+                if len(block) != read_size:
+                    raise IOError(f"Expected {read_size} bytes, but got {len(block)}")
+                self.bulk_write(self.pad_for_write(block))
+                yield block
+                length -= block_size
         finally:
             self.end_write()

src/jdmtool/main.py
@@ -25,7 +25,7 @@ import tqdm
 from .common import JdmToolException, get_data_dir
 from .config import get_config, get_config_file
 from .const import GRM_FEAT_KEY
-from .data_card.common import ProgrammingDevice, ProgrammingException
+from .data_card.common import DataCardType, ProgrammingDevice, ProgrammingException
 from .service import Service, ServiceException, SimpleService, get_downloads_dir, load_services
 
 
@@ -754,14 +754,18 @@ def _transfer_data_card(dev: ProgrammingDevice, service: Service, full_erase: bo
     card_size_min = int(service.get_property('media/card_size_min'))
     card_size_max = int(service.get_property('media/card_size_max'))
 
-    if not card_size_min <= dev.get_total_size() <= card_size_max:
+    assert dev.card_type is not DataCardType.NONE
+
+    if dev.card_type is DataCardType.TAWS:
+        print()
+        print("WARNING: You are trying to transfer a Nav database to a Terrain/Obstacles data card!")
+    elif not card_size_min <= dev.get_total_size() <= card_size_max:
         print()
         print(
             f"WARNING: This service requires a data card between "
             f"{card_size_min // 2**20}MB and {card_size_max // 2**20}MB, "
             f"but yours is {dev.get_total_size() // 2**20}MB!"
         )
-        print()
 
     print()
     print("Selected service:")
@@ -864,12 +868,16 @@ def cmd_detect(dev: ProgrammingDevice, verbose: bool) -> None:
 
 @with_data_card
 def cmd_read_database(dev: ProgrammingDevice, path: str, full_card: bool) -> None:
+    assert dev.card_type is not DataCardType.NONE
+
     start = time.perf_counter()
 
+    total_size = dev.get_total_size()
+
     with open(path, 'w+b') as fd:
-        with tqdm.tqdm(desc="Reading the database", total=dev.get_total_size(), unit='B', unit_scale=True) as t:
-            for block in dev.read_blocks(0, dev.get_total_sectors()):
-                if not full_card and block == b'\xFF' * ProgrammingDevice.BLOCK_SIZE:
+        with tqdm.tqdm(desc="Reading the database", total=total_size, unit='B', unit_scale=True) as t:
+            for block in dev.read_blocks(0, total_size):
+                if dev.card_type is DataCardType.NAVDATA and not full_card and not block.strip(b'\xFF'):
                     # Garmin card has no concept of size of the data,
                     # so we stop when we see a completely empty block.
                     break
@@ -889,49 +897,54 @@ def _write_database(dev: ProgrammingDevice, path: str, full_erase: bool) -> None
         if size > max_size:
             raise ProgrammingException(f"Database file is too big: {size}! The maximum size is {max_size}.")
 
-        def _read_block():
-            return fd.read(ProgrammingDevice.BLOCK_SIZE).ljust(ProgrammingDevice.BLOCK_SIZE, b'\xFF')
-
-        sectors_required = -(-size // ProgrammingDevice.SECTOR_SIZE)
-        total_size = sectors_required * ProgrammingDevice.SECTOR_SIZE
-
         if full_erase:
             sectors_to_erase = dev.get_total_sectors()
         else:
             # Erase an extra sector just to be safe.
+            sectors_required = -(-size // dev.card_type.sector_size)
             sectors_to_erase = min(sectors_required + 1, dev.get_total_sectors())
-        total_erase_size = sectors_to_erase * ProgrammingDevice.SECTOR_SIZE
 
-        sector_is_blank = [True] * sectors_to_erase
+        total_erase_size = sectors_to_erase * dev.card_type.sector_size
 
-        with tqdm.tqdm(desc="Blank checking", total=total_erase_size, unit='B', unit_scale=True) as t:
-            for sector_idx in range(sectors_to_erase):
-                for i, block in enumerate(dev.read_blocks(sector_idx, 1)):
-                    if block != b'\xff' * ProgrammingDevice.BLOCK_SIZE:
-                        sector_is_blank[sector_idx] = False
-                        t.update(ProgrammingDevice.BLOCK_SIZE * (ProgrammingDevice.BLOCKS_PER_SECTOR - i))
-                        break
+        # For NavData cards, check if sectors are already blank before erasing.
+        # For TAWS cards, it is much faster to just erase everything.
+        check_blanks = dev.card_type is DataCardType.NAVDATA
 
-                    t.update(ProgrammingDevice.BLOCK_SIZE)
+        if check_blanks:
+            sector_is_blank = [True] * sectors_to_erase
 
-        # Data card can only write by changing 1s to 0s (effectively doing a bit-wise AND with
-        # the existing contents), so all data needs to be "erased" first to reset everything to 1s.
-        with tqdm.tqdm(desc="Erasing the database", total=total_erase_size, unit='B', unit_scale=True) as t:
-            for sector_idx in range(sectors_to_erase):
-                if not sector_is_blank[sector_idx]:
-                    for _ in dev.erase_sectors(sector_idx, 1):
-                        pass
-                t.update(ProgrammingDevice.SECTOR_SIZE)
+            with tqdm.tqdm(desc="Blank checking", total=total_erase_size, unit='B', unit_scale=True) as t:
+                for sector_idx in range(sectors_to_erase):
+                    remaining = dev.card_type.sector_size
+                    for block in dev.read_blocks(sector_idx, remaining):
+                        if block.strip(b'\xff'):
+                            sector_is_blank[sector_idx] = False
+                            t.update(remaining)
+                            break
 
-        with tqdm.tqdm(desc="Writing the database", total=total_size, unit='B', unit_scale=True) as t:
-            for _ in dev.write_blocks(0, sectors_required, _read_block):
-                t.update(ProgrammingDevice.BLOCK_SIZE)
+                        t.update(len(block))
+                        remaining -= len(block)
+
+            with tqdm.tqdm(desc="Erasing the database", total=total_erase_size, unit='B', unit_scale=True) as t:
+                for sector_idx in range(sectors_to_erase):
+                    if not sector_is_blank[sector_idx]:
+                        for _ in dev.erase_sectors(sector_idx, 1):
+                            pass
+                    t.update(dev.card_type.sector_size)
+        else:
+            with tqdm.tqdm(desc="Erasing the database", total=size, unit='B', unit_scale=True) as t:
+                for _ in dev.erase_sectors(0, sectors_to_erase):
+                    t.update(dev.card_type.sector_size)
+
+        with tqdm.tqdm(desc="Writing the database", total=size, unit='B', unit_scale=True) as t:
+            for block in dev.write_blocks(0, size, fd.read):
+                t.update(len(block))
 
         fd.seek(0)
 
-        with tqdm.tqdm(desc="Verifying the database", total=total_size, unit='B', unit_scale=True) as t:
-            for card_block in dev.read_blocks(0, sectors_required):
-                file_block = _read_block()
+        with tqdm.tqdm(desc="Verifying the database", total=size, unit='B', unit_scale=True) as t:
+            for card_block in dev.read_blocks(0, size):
+                file_block = fd.read(len(card_block))
 
                 if card_block != file_block:
                     raise ProgrammingException("Verification failed!")
@@ -940,6 +953,14 @@ def _write_database(dev: ProgrammingDevice, path: str, full_erase: bool) -> None
 
 @with_data_card
 def cmd_write_database(dev: ProgrammingDevice, path: str, full_erase: bool) -> None:
+    assert dev.card_type is not DataCardType.NONE
+
+    if dev.card_type is DataCardType.TAWS:
+        print()
+        print("WARNING: Terrain/Obstacles data cards are not yet supported!")
+        print("See https://github.com/dimaryaz/jdmtool/issues/28")
+        print()
+
     confirm(f"Transfer {path} to the data card?")
 
     start = time.perf_counter()
@@ -954,11 +975,11 @@ def cmd_write_database(dev: ProgrammingDevice, path: str, full_erase: bool) -> N
 
 def _clear_card(dev: ProgrammingDevice) -> None:
     sectors_to_erase = dev.get_total_sectors()
-    total_erase_size = sectors_to_erase * ProgrammingDevice.SECTOR_SIZE
+    total_erase_size = sectors_to_erase * dev.card_type.sector_size
 
     with tqdm.tqdm(desc="Erasing the database", total=total_erase_size, unit='B', unit_scale=True) as t:
         for _ in dev.erase_sectors(0, sectors_to_erase):
-            t.update(ProgrammingDevice.SECTOR_SIZE)
+            t.update(dev.card_type.sector_size)
 
 
 @with_data_card

commit 8dd79c800c5ce443caf6ed5ebf327c76a29907fc

Unbreak the Skybound programmer device
Got broken by 525dcd995f3ea69058c02f03cbe4978423edce1e

src/jdmtool/data_card/skybound.py
@@ -162,12 +162,23 @@ class SkyboundDevice(ProgrammingDevice):
         self.check_card()
 
     def read_blocks(self, start_sector: int, length: int) -> Generator[bytes, None, None]:
+        block_size = self.card_type.read_size
+        blocks_per_sector = self.card_type.sector_size // block_size
+
         self.before_read()
-        for sector in range(start_sector, start_sector + num_sectors):
-            self.select_sector(sector)
-            for i in range(self.BLOCKS_PER_SECTOR):
-                self._loop_helper(i)
-                yield self.read_block()
+        block_idx = 0
+
+        while length > 0:
+            if block_idx % blocks_per_sector == 0:
+                self.select_sector(start_sector + block_idx // blocks_per_sector)
+
+            self._loop_helper(block_idx)
+            block_idx += 1
+
+            block = self.read_block()
+            assert len(block) == block_size
+            yield block[:min(block_size, length)]
+            length -= block_size
 
     def erase_sectors(self, start_sector: int, num_sectors: int) -> Generator[None, None, None]:
         self.before_write()
@@ -180,12 +191,24 @@ class SkyboundDevice(ProgrammingDevice):
     def write_blocks(
         self, start_sector: int, length: int,
         read_func: Callable[[int], bytes]
-    ) -> Generator[None, None, None]:
+    ) -> Generator[bytes, None, None]:
+        block_size = self.card_type.max_write_size
+        blocks_per_sector = self.card_type.sector_size // block_size
+
         self.before_write()
-        for sector in range(start_sector, start_sector + num_sectors):
-            self.select_sector(sector)
-            for i in range(self.BLOCKS_PER_SECTOR):
-                self._loop_helper(i)
-                block = read_func()
-                self.write_block(block)
-                yield
+        block_idx = 0
+
+        while length > 0:
+            if block_idx % blocks_per_sector == 0:
+                self.select_sector(start_sector + block_idx // blocks_per_sector)
+
+            self._loop_helper(block_idx)
+            block_idx += 1
+
+            read_size = min(block_size, length)
+            block = read_func(read_size)
+            if len(block) != read_size:
+                raise IOError(f"Expected {read_size} bytes, but got {len(block)}")
+            self.write_block(self.pad_for_write(block))
+            yield block
+            length -= block_size